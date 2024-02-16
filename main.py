00a804aceb89ce5ec2b147a72c655b628447ee9c
Kirill Konovalov <kirill.konovalov@jora.com>
2024-02-16 12:19:19 +1100

    Tested the connections

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ from dotenv import load_dotenv
 import os
 import base64
 import json
-from requests import post
+from requests import post, get
+import urllib.parse
 
 load_dotenv()
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
+
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+
+auth_code = get(AUTH_URL, {
+    'client_id': client_id,
+    'response_type': 'code',
+    'redirect_uri': 'https://open.spotify.com/collection/playlists',
+    'scope': 'playlist-modify-private',
+})
 
 def get_token():
     auth_string = client_id + ':' + client_secret
@@ -28,10 +40,38 @@ def get_token():
     return token
 
 def get_auth_header(token):
-    return {'Authorisation' : 'Bearer ' + token}
+    return {'Authorization' : 'Bearer ' + token}
 
+def search_for_artist(token, artist_name):
+    url = 'https://api.spotify.com/v1/search'
+    headers = get_auth_header(token)
+    query = f'?q={artist_name}&type=artist&limit=1'
 
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)
+    print(json_result)
 
+def get_recently_played_tunes(token, before_date = None, after_date = None, limit = None):
+    url = 'https://api.spotify.com/v1/me/player/recently-played'
+    headers = get_auth_header(token)
+    
+    # Initialize parameters dictionary
+    params = {}
+    if before_date is not None:
+        params['before'] = before_date
+    if after_date is not None:
+        params['after'] = after_date
+    if limit is None:
+        params['limit'] = 50
 
-token = get_token()
-print(token)
+    result = get(url, headers=headers, params=params)
+    json_result = json.loads(result.content)
+    return json_result
+
+    result = get(url, header = headers)
+    json_result = json.loads(result.content)
+
+# token = get_token()
+
+print(get_token())
--- pull_the_data.py
+++ pull_the_data.py
@@ -1,7 +1,7 @@
 import spotipy
 from dotenv import load_dotenv
 import os
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
 scope = "user-read-recently-played"
 
@@ -10,26 +10,28 @@ load_dotenv()
 client_id =os.getenv('CLIENT_ID')
 client_secret =os.getenv('CLIENT_SECRET')
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    scope=scope,
-    client_id=os.getenv('CLIENT_ID'),
-    client_secret=os.getenv('CLIENT_SECRET'),
-    redirect_uri="http://localhost:9090/"  # This URI needs to be registered in your Spotify app settings
-))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+#     scope=scope,
+#     client_id=os.getenv('CLIENT_ID'),
+#     client_secret=os.getenv('CLIENT_SECRET'),
+#     redirect_uri="http://localhost:9090/"  # This URI needs to be registered in your Spotify app settings
+# ))
 
-tracks = sp.current_user_recently_played()
+# tracks = sp.current_user_recently_played()
 
-for i, item in enumerate(tracks['items']):
-    track = item['track']
-    print(f"{i+1} - {track['artists'][0]['name']} - {track['name']}")
+# print(tracks['items'])
+
+# for i, item in enumerate(tracks['items']):
+#     track = item['track']
+#     print(f"{i+1} - {track['artists'][0]['name']} - {track['name']}")
 
 
-# auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-# sp = spotipy.Spotify(auth_manager=auth_manager)
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# current_plays = sp.current_user_recently_played(limit = 50)
+current_plays = sp.current_user_recently_played(limit = 50)
 
-# print(current_plays)
+print(current_plays)
 
 # playlists = sp.user_playlists('spotify')
 # while playlists:
